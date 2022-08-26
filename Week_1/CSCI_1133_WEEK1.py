14ebbf7714816a0cfdbf1b01334e8e785fbf98fe
DxnielRavindran <bumbles0506@gmail.com>
2022-08-25 23:17:23 -0500

    messing around with classes

--- Week_1/CSCI_1133_WEEK1.py
+++ Week_1/CSCI_1133_WEEK1.py
@@ -1,5 +1,18 @@
 #hello world 
 
-print("hello world")
 
-print("test")
+class Employee:
+    def __init__(self,first,last,pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+    def full_name(self):
+        print('{} {}'.format(self.first,self.last))
+
+    
+        
+
+emp_1 = Employee('Daniel', 'Ravin', 6000,)
+print(emp_1.full_name())
